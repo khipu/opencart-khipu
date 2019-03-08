e010f385a5860ae3ca84ef12482fd790eb06994e
chaotive <leonardomontenegro123@gmail.com>
2019-03-08 16:31:47 -0300

    Updating khipu library to 2.8.2

--- catalog/controller/extension/payment/vendor/composer/autoload_real.php
+++ catalog/controller/extension/payment/vendor/composer/autoload_real.php
@@ -23,7 +23,7 @@ class ComposerAutoloaderInit1b437017407bc36c80391dd3fb92d50e
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit1b437017407bc36c80391dd3fb92d50e', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
 
